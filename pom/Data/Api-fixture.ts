82e53100cb50192ca52803ac5f8d1d36a142e47e
Juan Fernando <juanfer9810@gmail.com>
2026-03-19 17:30:19 -0500

    Test ok

--- pom/Data/Api-fixture.ts
+++ pom/Data/Api-fixture.ts
@@ -10,11 +10,12 @@ export const test = base.extend<ApiFixtures>({
     const headerName = process.env.API_HEADER!;
     const apiKey = process.env.API_KEY!;
 
+    // validate that the required environment variables are set
     if (!baseURL) throw new Error('missing  API_URL .env');
     if (!headerName ) throw new Error('missing API_HEADER .env');
     if (!apiKey) throw new Error('missing  API_KEY .env');
     
-    
+    // create a new APIRequestContext with the base URL and the API key in the headers
     const api = await request.newContext({
         baseURL: baseURL!,
         extraHTTPHeaders: {    
